e2f560fbf3f4669f163cdf1890dea8a59d102c9a
Muhammad Zaeem Khaliq <57555591+ZaeemKhaliq@users.noreply.github.com>
2020-07-01 18:59:04 +0500

    Dropdown file
    Script file for notifications dropdown

---
+++ Zaeem's Contribution/ADMIN PANEL/scripts/dropdown1.js
@@ -0,0 +1,19 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show1");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn1')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show1')) {
+        openDropdown.classList.remove('show1');
+      }
+    }
+  }
+}
