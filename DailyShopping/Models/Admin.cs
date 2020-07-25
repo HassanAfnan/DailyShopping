07612496bebd0673fbe8116453b31b0e01ed6b6f
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-25 17:17:51 +0500

    Backend Classes For The Projects Uptill Now

---
+++ DailyShopping/Models/Admin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DailyShopping.Models
+{
+    public class Admin
+    {
+        public string Aid { get; set; }
+
+        public string Name { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public string role = "Admin";
+
+        public void signUp(Users a)
+        {
+            Connection gcon = new Connection();
+            SqlParameter[] parameters = new SqlParameter[]
+                {
+                    new SqlParameter(){ ParameterName = "@name", Value = a.firstName },
+                    new SqlParameter(){ ParameterName = "@email", Value = a.email},
+                    new SqlParameter(){ ParameterName = "@pass", Value = a.password},
+                    new SqlParameter(){ ParameterName = "@role", Value = a.address},
+                };
+            gcon.ExecuteSP("AdminSignUpProcedure", parameters);
+        }
+
+    }
+}
---
+++ DailyShopping/Models/Products.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace DailyShopping.Models
+{
+    public class Products:ICrud<Products>
+    {
+        public string pid { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }
+        public int categoryID { get; set; }
+        public string path { get; set; }
+
+        public void Add(Products s)
+        {
+            Connection gcon = new Connection();
+            SqlParameter[] parameters = new SqlParameter[]
+                {
+                    new SqlParameter(){ ParameterName = "@name", Value = s.name },
+                    new SqlParameter(){ ParameterName = "@description", Value = s.description},
+                    new SqlParameter(){ ParameterName = "@price", Value = s.price},
+                    new SqlParameter(){ ParameterName = "@Qty", Value = s.quantity},
+                    new SqlParameter(){ ParameterName = "@image", Value = s.path},
+                    new SqlParameter(){ ParameterName = "@category", Value = s.categoryID},
+                };
+            gcon.ExecuteSP("InsertProductProcedure", parameters);
+        }
+
+        public void Delete(Products s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Products> GetIds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StringReader Recipt(Products a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Products Search(Products s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Products s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
