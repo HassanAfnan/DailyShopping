11deff1c0352aae0a620773f8abef12440267008
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-28 23:15:41 +0500

    Added Get details method

--- DailyShopping/Models/Products.cs
+++ DailyShopping/Models/Products.cs
@@ -9,7 +9,7 @@ namespace DailyShopping.Models
 {
     public class Products:ICrud<Products>
     {
-        public string pid { get; set; }
+        public int pid { get; set; }
         public string name { get; set; }
         public string description { get; set; }
         public int price { get; set; }
@@ -31,7 +31,24 @@ namespace DailyShopping.Models
                 };
             gcon.ExecuteSP("InsertProductProcedure", parameters);
         }
-
+        public Products GetDetails(Products s)
+        {
+            Connection con = new Connection();
+            SqlCommand com = new SqlCommand();
+            SqlDataReader rd;
+            com = new SqlCommand("select * from ProductView where Pid ='" + s.pid + "'", con.getConnection());
+            rd = com.ExecuteReader();
+            while (rd.Read())
+            {
+                s.name = rd["name"].ToString(); 
+                s.description = rd["description"].ToString();
+                s.price = Convert.ToInt32(rd["price"].ToString());
+                s.quantity = Convert.ToInt32(rd["quantity"].ToString());
+                s.path = rd["image"].ToString();
+                s.categoryID = Convert.ToInt32(rd["category"].ToString());
+            }
+            return s;
+        }
         public void Delete(Products s)
         {
             throw new NotImplementedException();
